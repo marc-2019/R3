3cc10f4a97b2f81903c2883dda7a1d9461e3e1d5
Marc <marc@ournewnormal.co.nz>
2024-11-05 22:06:06 +1300

    chore: remove services index while web3 is in components

--- src/services/index.ts
+++
@@ -1,4 +0,0 @@
-// src/services/index.ts
-export { Web3Service } from './web3/web3Service';
-export { NetworkType, type NetworkConfig, type ConnectionStatus } from './web3/types';
-export { NETWORK_CONFIGS } from './web3/constants';
