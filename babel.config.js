35e3eba46ad587b0c7b00d4354629f6a55af043b
Marc <marc@ournewnormal.co.nz>
2024-11-12 21:51:25 +1300

    Update Babel config to exclude problematic modules for Docker build

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-    presets: [
-      '@babel/preset-env', // Transforms modern JavaScript
-      '@babel/preset-typescript', // Transforms TypeScript files
-    ],
-  };
-  
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+  ],
+  ignore: [
+    'node_modules/process',  // Ignore process/browser.js during Babel transformation
+    'node_modules/web3',     // Ignore large libraries like web3 during transformation to prevent performance issues
+  ],
+};
