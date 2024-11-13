edba75c40bf07a50aa1a69ecc6beb6d956f0e688
Marc <marc@ournewnormal.co.nz>
2024-11-13 15:51:49 +1300

    Fix: Remove Jest config from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,11 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', {
-      tsconfig: 'tsconfig.json',
-      isolatedModules: true
-    }]
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(jose|next-auth)/)'
-  ],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|sass|scss)$': 'identity-obj-proxy'
-  },
-  setupFilesAfterEnv: [
-    '<rootDir>/jest.setup.js'
-  ],
-  testPathIgnorePatterns: [
-    '<rootDir>/.next/',
-    '<rootDir>/node_modules/'
-  ]
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add any Next.js specific configuration here
+  // For example:
+  // poweredByHeader: false,
+  // compress: true,
 };
+
+module.exports = nextConfig;
